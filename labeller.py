8c6eeaa8e87d8e9b95047f21d6554cfd29090bf8
Samuel Strong <sastrong@jpl.nasa.gov>
2023-08-22 01:23:10 -0700

    Prototype complete with bounding box adding, removing, saving files, and image selector UI

--- labeller.py
+++ labeller.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import os
+from flask import request, jsonify
 import json
 
 app = Flask(__name__, static_url_path='', static_folder=os.path.join(os.path.dirname(__file__), '..'))
@@ -16,6 +17,22 @@ def home():
         bboxes = labels.get('bboxes', [])
 
     return render_template('index.html', image_index=image_index, bboxes=bboxes)
+
+@app.route('/save_bboxes/<int:image_index>', methods=['POST'])
+def save_bboxes(image_index):
+    try:
+        data = request.json
+        bboxes = data['bboxes']
+        filename = os.path.join('../labels', '%03d.json' % image_index)
+        print(f"Saving to {filename} with bboxes {bboxes}")  # Debug print
+
+        with open(filename, 'w') as file:
+            json.dump({'bboxes': bboxes}, file)
+
+        return jsonify(success=True)
+    except Exception as e:
+        print(f"Error: {e}")  # Logging the error can help in debugging
+        return jsonify(success=False), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
